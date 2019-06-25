e7ec4252c38ff8b29163309f012e6f5d5ef70324
Rafael Nunes Santana <39140806+RafRunner@users.noreply.github.com>
2019-06-24 21:50:09 -0300

    Removendo teste

--- src/Main/Main.groovy
+++ src/Main/Main.groovy
@@ -1,15 +1,8 @@
 package Main
 
 import Controllers.MenuIniciar
-import View.Condicao2ApresentacaoView
-import View.Condicao2View
-import View.ViewUtils
 
-import javax.swing.JFrame
 import javax.swing.JOptionPane
-import java.awt.Color
-import java.awt.Dimension
-import java.awt.Toolkit
 
 class Main {
 
@@ -22,15 +15,5 @@ class Main {
                 JOptionPane.showMessageDialog(null, mensagemErro, 'Erro!', JOptionPane.ERROR_MESSAGE)
             }
         })
-//        teste()
-    }
-
-    static void teste() {
-        JFrame janela = new JFrame()
-        Condicao2ApresentacaoView condicao2ApresentacaoView = new Condicao2ApresentacaoView(Color.YELLOW, 'Teste', 'instrução teste', new Object())
-        Condicao2View condicao2View = new Condicao2View(['Ola'], Color.PINK, 'Teste', null, new Object())
-        janela.add(condicao2View)
-        Dimension tamanhoTela = Toolkit.defaultToolkit.screenSize
-        ViewUtils.configuraJFrame(janela, tamanhoTela, 'teste')
     }
 }
